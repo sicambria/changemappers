6d4a951de7f734e5908a27a24feb7685e5d044b7
rameezamemon1 <rameeza.memon1@gmail.com>
2021-03-26 02:30:13 +0500

    package.json changed

--- changemappers-frontend/src/server.js
+++ changemappers-frontend/src/server.js
@@ -2,7 +2,7 @@ import sirv from "sirv";
 import polka from "polka";
 import compression from "compression";
 import bodyParser from "body-parser";
-import session from "express-session";
+import session from "cookie-session";
 import sessionFileStore from "session-file-store";
 import * as sapper from "@sapper/server";
 
